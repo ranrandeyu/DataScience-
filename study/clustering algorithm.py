ec5854975e9b6df8539947d3e6a7874338fc1084
Laceyrran <34807467+ranrandeyu@users.noreply.github.com>
2018-10-15 20:14:43 +0800

    Update and rename unsupervised leaning.py to clustering algorithm.py

--- study/clustering algorithm.py
+++ study/clustering algorithm.py
@@ -1,34 +1,4 @@
-#层次方法：聚类的训练和对比
-#从均值为u1=[10,0] 和u2=[0,10],方差为[3 1 的两个二维正态分布，随机选取数据点形成数据集
-#                                   1 4 ]
-from matplotlib import pyplot as plt
-import numpy as np
 
-np.random.seed(4711)
-c1 = np.random.multivariate_normal([10, 0], [[3, 1], [1, 4]], size=[100,])
-l1 = np.zeros(100)
-l2 = np.ones(100)
-c2 = np.random.multivariate_normal([0, 10], [[3, 1], [1, 4]], size=[100,])
-
-np.random.seed(1)
-noise1x = np.random.normal(0,2,100)
-noise1y = np.random.normal(0,8,100)
-noise2 = np.random.normal(0,8,100)
-c1[:,0] += noise1x
-c1[:,1] += noise1y
-c2[:,1] += noise2
-
-fig = plt.figure(figsize=(20,15))
-ax = fig.add_subplot(111)
-ax.set_xlabel('x',fontsize=30)
-ax.set_ylabel('y',fontsize=30)
-fig.suptitle('classes',fontsize=30)
-labels = np.concatenate((l1,l2),)
-X = np.concatenate((c1, c2),)
-pp1= ax.scatter(c1[:,0], c1[:,1],cmap='prism',s=50,color='r')
-pp2= ax.scatter(c2[:,0], c2[:,1],cmap='prism',s=50,color='g')
-ax.legend((pp1,pp2),('class 1', 'class2'),fontsize=35)
-fig.savefig('classes.png')
 #层次方法：聚类的训练和对比
 #从均值为u1=[10,0] 和u2=[0,10],方差为[3 1 的两个二维正态分布，随机选取数据点形成数据集
 #                                     1 4 ]
