f65dd3564e2b5ff135b50dbd201daeb62b6501ab
Laceyrran <34807467+ranrandeyu@users.noreply.github.com>
2018-10-15 19:30:09 +0800

    Update unsupervised leaning.py

--- study/unsupervised leaning.py
+++ study/unsupervised leaning.py
@@ -29,3 +29,75 @@ pp1= ax.scatter(c1[:,0], c1[:,1],cmap='prism',s=50,color='r')
 pp2= ax.scatter(c2[:,0], c2[:,1],cmap='prism',s=50,color='g')
 ax.legend((pp1,pp2),('class 1', 'class2'),fontsize=35)
 fig.savefig('classes.png')
+#层次方法：聚类的训练和对比
+#从均值为u1=[10,0] 和u2=[0,10],方差为[3 1 的两个二维正态分布，随机选取数据点形成数据集
+#                                     1 4 ]
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.cluster.hierarchy import fcluster
+from sklearn.cluster import KMeans
+from sklearn.cluster import MeanShift
+from sklearn import mixture
+from scipy.cluster.hierarchy import linkage
+import sklearn.mixture
+
+np.random.seed(4711)
+c1 = np.random.multivariate_normal([10, 0], [[3, 1], [1, 4]], size=[100,])
+l1 = np.zeros(100)
+l2 = np.ones(100)
+c2 = np.random.multivariate_normal([0, 10], [[3, 1], [1, 4]], size=[100,])
+
+np.random.seed(1)
+noise1x = np.random.normal(0,2,100)
+noise1y = np.random.normal(0,8,100)
+noise2 = np.random.normal(0,8,100)
+c1[:,0] += noise1x
+c1[:,1] += noise1y
+c2[:,1] += noise2
+
+fig = plt.figure(figsize=(20,15))
+ax = fig.add_subplot(111)
+ax.set_xlabel('x',fontsize=30)
+ax.set_ylabel('y',fontsize=30)
+fig.suptitle('classes',fontsize=30)
+labels = np.concatenate((l1,l2),)
+X = np.concatenate((c1, c2),)
+pp1= ax.scatter(c1[:,0], c1[:,1],cmap='prism',s=50,color='r')
+pp2= ax.scatter(c2[:,0], c2[:,1],cmap='prism',s=50,color='g')
+ax.legend((pp1,pp2),('class 1', 'class2'),fontsize=35)
+fig.savefig('classes.png')
+
+
+from scipy.cluster.hierarchy import fcluster
+from sklearn.cluster import KMeans
+from sklearn.cluster import MeanShift
+from sklearn import mixture
+from scipy.cluster.hierarchy import linkage
+fig, ((axis1, axis2), (axis3,axis4)) = plt.subplots(2, 2, sharex='col', sharey='row')
+
+#k-means（k均值）
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(X)
+pred_kmeans = kmeans.labels_
+plt.scatter(X[:,0], X[:,1], c=kmeans.labels_, cmap='prism')  # plot points with cluster dependent colors
+axis1.scatter(X[:,0], X[:,1], c=kmeans.labels_, cmap='prism')
+axis1.set_ylabel('y',fontsize=40)
+axis1.set_title('k-means',fontsize=20)
+
+#mean-shift（均值漂移）
+ms = MeanShift(bandwidth=7)
+ms.fit(X)
+pred_ms = ms.labels_
+axis2.scatter(X[:,0], X[:,1], c=pred_ms, cmap='prism')
+axis2.set_title('mean-shift',fontsize=20)
+
+
+#hierarchical（层次算法）
+Z = linkage(X, 'ward')
+max_d = 110
+pred_h = fcluster(Z, max_d, criterion='distance')
+axis3.scatter(X[:,0], X[:,1], c=pred_h, cmap='prism')
+axis3.set_xlabel('x',fontsize=40)
+axis3.set_title('hierarchical ward',fontsize=20)
+fig.set_size_inches(18.5,10.5)
+fig.savefig('comp_clustering.png', dpi=100)
