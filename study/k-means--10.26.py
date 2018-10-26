7f6bbb321119aebbe1c3412b16ecdbb23b3b978b
Laceyrran <34807467+ranrandeyu@users.noreply.github.com>
2018-10-26 23:56:57 +0800

    Create k-means--10.26.py

---
+++ study/k-means--10.26.py
@@ -0,0 +1,53 @@
+import numpy as np
+from sklearn.cluster import KMeans
+data = np.random.rand(100, 3) #生成一个随机数据，样本大小为100, 特征数为3
+
+#假如我要构造一个聚类数为3的聚类器
+estimator = KMeans(n_clusters=3)#构造聚类器
+estimator.fit(data)#聚类
+label_pred = estimator.labels_ #获取聚类标签
+centroids = estimator.cluster_centers_ #获取聚类中心
+inertia = estimator.inertia_ # 获取聚类准则的总和
+
+from sklearn.externals import joblib
+ 
+# 注释语句用来存储你的模型
+joblib.dump(km,  'doc_cluster.pkl')
+km = joblib.load('doc_cluster.pkl')
+clusters = km.labels_.tolist()
+
+frame = pd.DataFrame(films, index = [clusters] , columns = ['rank', 'title', 'cluster', 'genre'])
+frame['cluster'].value_counts()
+
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import MiniBatchKMeans, KMeans
+from sklearn.metrics.pairwise import pairwise_distances_argmin
+from sklearn.datasets.samples_generator import make_blobs
+
+# 获取数据
+np.random.seed(0)
+
+batch_size = 45
+centers = [[1, 1], [-1, -1], [1, -1]]
+n_clusters = len(centers)
+X, labels_true = make_blobs(n_samples=3000, centers=centers, cluster_std=0.7)
+
+# kmeans
+# Compute clustering with Means
+
+k_means = KMeans(init='k-means++', n_clusters=3, n_init=10)
+t0 = time.time()
+k_means.fit(X)
+t_batch = time.time() - t0
+
+# MiniBatchKMeans
+
+mbk = MiniBatchKMeans(init='k-means++', n_clusters=3, batch_size=batch_size,
+                      n_init=10, max_no_improvement=10, verbose=0)
+t0 = time.time()
+mbk.fit(X)
+t_mini_batch = time.time() - t0
